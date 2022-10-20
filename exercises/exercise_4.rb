e0c1f871e429da92d4da3cffd067c7f5cf1e1415
NadiaPia <206i417@gmail.com>
2022-10-20 19:32:11 +0000

    Loading a subset of stores, usint the Where method for select

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,41 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+store = Store.create(
+  name: "Hope", 
+  annual_revenue: 224000, 
+  mens_apparel: false, 
+  womens_apparel: true
+  )
+
+store = Store.create(
+  name: "Whistler", 
+  annual_revenue: 1900000, 
+  mens_apparel: true, 
+  womens_apparel: false
+  )
+
+store = Store.create(
+  name: "Yaletown", 
+  annual_revenue: 430000, 
+  mens_apparel: true, 
+  womens_apparel: true
+  )
+
+stores = Store.all
+#puts stores.count
+
+@mens_stores = Store.where(mens_apparel: true)
+#puts @mens_stores.inspect
+
+@mens_stores.each do |shop|
+  puts "#{shop.name}, #{shop.annual_revenue}"
+end
+
+@womens_stores = Store.where("womens_apparel = true AND annual_revenue < 1000000")
+@womens_stores.each do |shop|
+  puts "#{shop.name}, #{shop.annual_revenue}"
+end
+
+
