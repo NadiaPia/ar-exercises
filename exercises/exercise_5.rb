e1abf6930d818f8680bdb4a8b31de138e520d1f6
NadiaPia <206i417@gmail.com>
2022-10-21 00:20:07 +0000

    Calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total = Store.sum(:annual_revenue)
+average_amount = Store.average(:annual_revenue)
+top_sales = Store.where("annual_revenue >1000000").count
+puts "total is #{total}"
+puts "average is #{average_amount}"
+puts "top_sales are #{top_sales}"
